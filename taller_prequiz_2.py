f56a1be3a2d66e6926cb2bbc99b2a212e9046069
MarianaGG3 <mariana.giraldog@udea.edu.co>
2024-04-11 09:26:35 -0500

    7. funciones: suma,resta.mult,div,desviacion estandar, promedio en numpy

--- taller_prequiz_2.py
+++ taller_prequiz_2.py
@@ -14,7 +14,8 @@ mtz=np.random.randint(1000, size=(200, 200, 30,1))
 #2:quitar una dimension=dim 3
 a=np.copy(mtz)
 b=a.reshape(200,200,30)
-#print(b.ndim)
+print(b.shape)
+print(b.ndim)
 
 #3: mostrar atributos de la matriz 3D(tamaño,forma,dimension)
 print(b.size)
@@ -22,11 +23,11 @@ print(b.ndim)
 print(b.shape)
 print(b.dtype)
 
-#4:pasar matriz a 2D
+# #4:pasar matriz a 2D
 m2=b.reshape(4000,300)
 print(m2.size) #se cambia la forma, pero se conserva el tamaño
 
-#5: pasar datos de matriz a dataframe
+# #5: pasar datos de matriz a dataframe
 def pasar_df(m2):
 
     df=pd.DataFrame(m2)
@@ -39,10 +40,24 @@ def cargar(a):
         return (arch_cargar)
 
     else:
-        a.endswith(".csv"):
         cargar_csv=pd.read_csv(a)
         return(cargar_csv)
 
-#7: funciones suma,resta  
+#7: funciones suma,resta, multiplicacion, division, desviacion estandar, promedio
+def sumar(datos, eje):
+    np.sum(datos, axis=eje)  
+
+def restar (datos, eje):
+    np.subtract(datos, axis=eje)
+
+def dividir(datos, eje):
+    np.divide(datos, axis=eje)
+
+def promedio(datos, eje):
+    np.mean(datos, axis=eje)
+
+def desviac_estandar(datos, eje):
+    np.std(datos, axis=eje)
     
+
 
