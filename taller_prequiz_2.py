0fdbaea0e65d6417c95bdf07a6e5a931c914988d
MarianaGG3 <mariana.giraldog@udea.edu.co>
2024-04-11 08:37:19 -0500

    se redujo la dimension de la misma matriz a 3, con el metodo copy, y reshape

--- taller_prequiz_2.py
+++ taller_prequiz_2.py
@@ -4,4 +4,9 @@ import matplotlib as plt
 
 #1: matriz aleatoria
 mtz=np.random.randint(1000, size=(200, 200, 30,1))
-print(mtz.size)
+#print(mtz.size)
+
+#2:quitar una dimension=dim 3
+a=np.copy(mtz)
+b=a.reshape(200,200,30)
+print(b.ndim)
