1c8d61fc095e7a8300384b75eee0744e3b77043f
MarianaGG3 <mariana.giraldog@udea.edu.co>
2024-04-11 08:41:13 -0500

    atributos de la matriz 3D (tamaño, dimension, forma,tipo)

--- taller_prequiz_2.py
+++ taller_prequiz_2.py
@@ -9,4 +9,10 @@ mtz=np.random.randint(1000, size=(200, 200, 30,1))
 #2:quitar una dimension=dim 3
 a=np.copy(mtz)
 b=a.reshape(200,200,30)
+#print(b.ndim)
+
+#3: mostrar atributos de la matriz 3D(tamaño,forma,dimension)
+print(b.size)
 print(b.ndim)
+print(b.shape)
+print(b.dtype)
